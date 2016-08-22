041e8a28d117f82ed86e59532905e21ba01d427e
Richard Bornat <richard@bornat.me.uk>
2016-08-22 14:16:44 +0100

    switched to using the parseutils stuff, I think

--- tolatex.ml
+++ tolatex.ml
@@ -11,46 +11,32 @@ open Arg
  
 exception Error of string
 
-let parse opts usage filename =
-  let in_channel = open_in filename in
-  let lexbuf = Lexing.from_channel in_channel in
-  try
-    let result = Parser.program Lexer.make_token lexbuf in
-    close_in in_channel; 
-    result
-  with
-  | Parsing.Parse_error ->
-      (close_in in_channel;
-       let curr = lexbuf.Lexing.lex_curr_p in
-       raise (Error ("**Parse error at line "^string_of_int (curr.Lexing.pos_lnum)^ 
-                     " character "^string_of_int (curr.Lexing.pos_cnum-curr.Lexing.pos_bol)^
-                     " (just before \""^Lexing.lexeme lexbuf^"\")"))
-                    )
-  | Program.ParseError(spos,s) ->
-        (close_in in_channel;
-         raise (Error ("\n**SYNTAX ERROR at "^string_of_sourcepos spos ^ ": " ^ s))
-        )
-  | Lexer.LexError(spos,s) -> 
-        (close_in in_channel;
-         raise (Error ("\n**LEXING ERROR at "^string_of_sourcepos spos ^ ": " ^ s))
-        )
-  | exn -> (close_in in_channel; raise exn)
+let parse filename =
+  (try Parseutils.parse_program filename
+   with 
+   | Parseutils.Error s -> raise (Error s)
+   | exn                -> raise exn
+  )
 
+let dot = ref false
 let multilace = ref false
 
-let tranfile opts usage filename = 
+let tranfile filename = 
   print_newline (print_string ("\nprocessing " ^ filename));
   let tidyup s = 
     print_string s;
     flush stdout;
     flush stderr
   in
-  try let prog = parse opts usage filename in
+  try let prog = parse filename in
       let show s = Printf.printf "\n\n%s\n" s in
+      if !dot then
+        show (Dotdiagram.dot_of_program filename prog)
+      else
       if !multilace then
         (nolace (); show (latex_of_program filename prog);
          laceonly (); show (latex_of_program filename prog);
-         embroider (); show(latex_of_program filename prog)
+         embroider (); show (latex_of_program filename prog)
         )
       else
         show (latex_of_program filename prog);
@@ -72,6 +58,7 @@ let opts = Arg.align [("-name"     , Tuple [Set_string snamesource; String sname
                       ("-thread"   , Tuple [Set_int tnamesource; String tnametarget],
                                                      " define thread names");
                       ("-cols"     , Set twocols   , " threads in columns (happens anyway with laceonly) (default no cols)");
+                      ("-dot"      , Set dot       , " dot diagram of constraints");
                       ("-noaux"    , Clear aux     , " don't show auxiliaries in laceonly and no lace");
                       ("-nolace"   , Unit nolace   , " no lacing");
                       ("-lace"     , Unit laceonly , " lace only");
@@ -82,4 +69,4 @@ let _ = Arg.parse opts (fun s -> files := s :: !files) usage
 let _ = match !files with 
         | [] -> print_string ("\nno file specified") 
         | fs -> Settings.expand_macros := false; 
-                List.iter (tranfile opts usage) (List.rev fs)
+                List.iter tranfile (List.rev fs)
