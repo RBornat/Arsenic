89cf37e9bf049aee499ba81970b25474398abd2b
Richard Bornat <richard@bornat.me.uk>
2016-08-22 12:53:49 +0100

    when I remember what Compile does I will remember what this is for

--- parseutils.ml
+++ parseutils.ml
@@ -3,7 +3,11 @@
    Licensed under the MIT license (sic): see LICENCE.txt or
    https://opensource.org/licenses/MIT
  *)
- 
+
+open Sourcepos
+
+exception Error of string
+
 (* ********************* nowhere else to put this ******************************* *)
 
 let parse_formula default string =
@@ -36,3 +40,30 @@ let parse_formula default string =
          )
 
 let parse_axiom = parse_formula (Some Formula._recTrue)
+
+(* ********************* what Arsenic, ToLatex and Compile fall back on ******************************* *)
+
+let parse_program filename =
+  let in_channel = open_in filename in
+  let lexbuf = Lexing.from_channel in_channel in
+  try
+    let result = Parser.program Lexer.make_token lexbuf in
+    close_in in_channel; 
+    result
+  with
+  | Parsing.Parse_error ->
+      (close_in in_channel;
+       let curr = lexbuf.Lexing.lex_curr_p in
+       raise (Error ("**Parse error at line "^string_of_int (curr.Lexing.pos_lnum)^ 
+                     " character "^string_of_int (curr.Lexing.pos_cnum-curr.Lexing.pos_bol)^
+                     " (just before \""^Lexing.lexeme lexbuf^"\")"))
+                    )
+  | Program.ParseError(spos,s) ->
+        (close_in in_channel;
+         raise (Error ("\n**SYNTAX ERROR at "^string_of_sourcepos spos ^ ": " ^ s))
+        )
+  | Lexer.LexError(spos,s) -> 
+        (close_in in_channel;
+         raise (Error ("\n**LEXING ERROR at "^string_of_sourcepos spos ^ ": " ^ s))
+        )
+  | exn -> (close_in in_channel; raise exn)
