452256bc5cb02ce3fd140fbc2324a9efcf4dfc77
Richard Bornat <richard@bornat.me.uk>
2016-08-22 12:39:32 +0100

    better treatment of usage defaults

--- usage.ml
+++ usage.ml
@@ -13,39 +13,42 @@ let usage = "Usage: " ^ progname ^ " [options]* filename filename ..."
 let ignored = ref ""
 let opts = Arg.align
                [("-verbose", Arg.Symbol (List.map fstof2 verboseopts, setverbose), 
-                   " verbose proofcheck, defaults false");
+                   " verbose proofcheck, various arguments, defaults false" );
                 ("suppresswarnings", Arg.Clear Report.show_warnings, " suppress warning messages");
                 ("-allvalid", Arg.Set all_valid, " accept all proof dependencies");
                 ("-laceonly", Arg.Set lace_only, " lace only, no proof");
                 ("-maxerrors", Arg.Set_int maxerrors, 
                     " quit after this number of Z3 errors or unknowns");
                 ("-spunchanged", Arg.Bool (set_bool sp_unchanged), 
-                    " conjoin x=(-x) in sp for unchanged x (default true)");
+                    Printf.sprintf " conjoin x=(-)x in sp for unchanged x (default %B)" !sp_unchanged);
                 ("-noproof", Arg.Set lace_only, " lace only, no proof");
-                ("-sat", Arg.Bool (set_bool param_usesat), " use sat check in stability (default true)");
-                ("-SCreg", Arg.Bool (set_bool param_SCreg), " register renaming (default true)");
-                ("-SCloc", Arg.Bool (set_bool param_SCloc), " SC per location (default true)");
-                ("-LocalSpec", Arg.Bool (set_bool param_LocalSpec), " don't speculate propagation (default true)");
+                ("-SCreg", Arg.Bool (set_bool param_SCreg), 
+                    Printf.sprintf " register renaming (default %B)" !param_SCreg);
+                ("-SCloc", Arg.Bool (set_bool param_SCloc), 
+                    Printf.sprintf " SC per location (default %B)" !param_SCloc);
+                ("-LocalSpec", Arg.Bool (set_bool param_LocalSpec), 
+                    Printf.sprintf " don't speculate propagation (default %B)" !param_LocalSpec);
                 ("-treeformulas", Arg.Bool (set_bool tree_formulas), 
-                    " print big formulas as trees in error reports (default true)");
+                    Printf.sprintf " print big formulas as trees in error reports (default %B)" !tree_formulas);
                 ("-tree_formulas", Arg.Bool (set_bool tree_formulas), 
-                    " print big formulas as trees in error reports (default true)");
+                    Printf.sprintf " print big formulas as trees in error reports (default %B)" !tree_formulas);
                 ("-simpleUBsince", Arg.Bool (set_bool simpleUBsince), 
-                    " use simple, but slower, Univ/B-> since translation (default false)");
+                    Printf.sprintf " use simple, but slower, Univ/B-> since translation (default %B)" !simpleUBsince);
                 ("-z3benchmarks", Arg.Bool (set_bool z3benchmarks), 
-                    " produce .smt2 files for each Z3 query (default false)");
+                    Printf.sprintf " produce .smt2 files for each Z3 query (default %B)" !z3benchmarks);
                 ("-z3onecontext", Arg.Set z3onecontext, 
                     " use the same context repeatedly, until a timeout");
                 ("-z3timeout", Arg.Set_int z3timeout, 
-                    " Z3 timeout setting, in milliseconds (default 4000)");
+                    Printf.sprintf " Z3 timeout setting, in milliseconds (default %d)" !z3timeout);
                 ("-z3track", Arg.Symbol (List.map fstof2 z3trackopts, setz3track), 
                     " show z3 enquiries even when not verbose");
                 ("-z3usenonlinear", Arg.Set z3usenonlinear, 
                     " use the non-linear arithmetic solver");
                 ("-z3useqe", Arg.Bool (set_bool z3useqe), 
-                    " use qe tactic (default true)");
+                    Printf.sprintf " use qe tactic (default %B)" !z3useqe);
                 ("-z3log"    , Arg.Set    keep_log   , " open Z3 log");
-                ("-z3counters", Arg.Set z3counters   , " show Z3 (counter) models (default false)");
+                ("-z3counters", Arg.Set z3counters   , 
+                    Printf.sprintf " show Z3 (counter) models (default %B)" !z3counters);
                 ("-error"  , Arg.Tuple [Arg.Set_string ignored; Arg.Set_string ignored],
                              " ignored (flag for Test)")
                 
